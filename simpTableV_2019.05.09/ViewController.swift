cd6f8bae2273a40013ced49c574bda65ff979172
dit08 <dit@imac-3.local>
2019-05-09 12:22:11 +0900

    Initial Commit

---
+++ simpTableV_2019.05.09/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simpTableV_2019.05.09
+//
+//  Created by dit08 on 2019. 5. 9..
+//  Copyright © 2019년 dit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
